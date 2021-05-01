429780c3b1e4f42811644057d325c4bb4acefb88
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-01 16:09:48 +0300

    feat: add app init. and redirect if not authorities

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {AppBar, Button, Container, IconButton, Toolbar, Typography} from '@material-ui/core';
+import {AppBar, Button, CircularProgress, Container, IconButton, Toolbar, Typography} from '@material-ui/core';
 import {Menu} from '@material-ui/icons';
 import {TodolistsList} from '../features/Todolists/TodolistsList';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from './store';
 import {RequestStatusType} from './app-reducer';
 import ErrorSnackbar from '../components/ErrorSnackbar/ErrorSnackbar';
 import Login from '../features/Login/Login';
 import {Route, Switch, Redirect} from 'react-router-dom';
+import {appInitTC, logOutTC} from '../features/Login/auth-reducer';
 
 // export type TaskStateType = {
 //     [todoListID: string]: Array<TaskType>
 // }
 
 function App() {
-
+    const dispatch = useDispatch();
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+    const appInit = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
+
+    useEffect(() => {
+        dispatch(appInitTC())
+    }, [])
+
     // const todoListID1 = v1()
     // const todoListID2 = v1()
     // const [todoLists, dispatchToTodolists] = useReducer(todoListReducer, [
@@ -36,6 +44,16 @@ function App() {
     //         {title: 'Meat', isDone: true, id: v1()},],
     // })
 
+    const onLogoutHandler = () => {
+        dispatch(logOutTC());
+    }
+
+    if (!appInit) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
 
     return (
         <div className="App">
@@ -47,7 +65,7 @@ function App() {
                     <Typography variant="h6">
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Button color="inherit" onClick={onLogoutHandler}>{isLoggedIn ? 'Log out' : 'Log in'}</Button>
                 </Toolbar>
             </AppBar>
             {status === 'loading' && <LinearProgress color="secondary"/>}
