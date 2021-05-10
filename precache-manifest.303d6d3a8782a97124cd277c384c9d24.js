e2b80c287b5687b498a64d506234613fd5718f07
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-10 16:34:37 +0300

    Updates

---
+++ precache-manifest.303d6d3a8782a97124cd277c384c9d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d7d2e4a70e2e37dabbdb2e6a67610a",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "1a84a798b0b55f689b9e",
+    "url": "/todolist-ts/static/css/main.93600c51.chunk.css"
+  },
+  {
+    "revision": "76c5adad09166d712ce1",
+    "url": "/todolist-ts/static/js/2.1621c81c.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/todolist-ts/static/js/2.1621c81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a84a798b0b55f689b9e",
+    "url": "/todolist-ts/static/js/main.bc631cf4.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
